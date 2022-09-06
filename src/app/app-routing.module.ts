9cbbec2fec12dc86ff7cf2a3309341bd864058ef
ManhcuongG1 <tranmanhcuongqbdl1@gmail.com>
2022-09-06 09:52:06 +0700

    cuongtm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {DetailComponent} from "./detail/detail.component";
+import {LoginComponent} from "./login/login.component";
+import {CartComponent} from "./cart/cart.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home-page' , component: HomePageComponent
+  },
+  {
+    path: 'detail' , component: DetailComponent
+  },{
+    path: 'login' , component: LoginComponent
+  },{
+    path: 'cart' , component: CartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
